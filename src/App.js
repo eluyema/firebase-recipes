d152e382297d916afd06f4c3a4b5bf3adfb0eaf2
eluyemaStudy <70893846+eluyema@users.noreply.github.com>
2022-04-26 04:05:13 +0300

    add auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import './App.css';
+import LoginForm from './components/LoginForm';
+
+import FirebaseAuthService from './FirebaseAuthService';
 
 function App() {
+  const [user, setUser] = useState(null);
+  
+  FirebaseAuthService.subscribeToAuthChanges(setUser);
+
   return (
     <div className="App">
       <div className='title-row'>
         <h1 className="title">Firebase recipes</h1>
+        <LoginForm existingUser={user}/>
       </div>
 
     </div>
